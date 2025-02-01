649179e12efe33de8638e927d030c407b29a4aa8
Arminas <arminas@ver.lt>
2025-02-01 14:22:56 +0200

    Build firmware

--- extracted/vendor/bin/install-recovery.sh
+++ extracted/vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):81788928:1e0757a3d043979012e1f22b77528bf3aceba080; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):81788928:6287bceb89e6d42e101db42809fb2881488c19dc; then
   applypatch \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):100663296:3eb70d7702899ba6601906798babca8c15cbd985 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):81788928:1e0757a3d043979012e1f22b77528bf3aceba080 && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):100663296:2c8c493a53871aa5b2eb422c28bff04aa40f9663 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):81788928:6287bceb89e6d42e101db42809fb2881488c19dc && \
       (/vendor/bin/log -t install_recovery "Installing new recovery image: succeeded" && setprop vendor.ota.recovery.status 200) || \
       (/vendor/bin/log -t install_recovery "Installing new recovery image: failed" && setprop vendor.ota.recovery.status 454)
 else
